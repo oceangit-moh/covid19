47f19c6328420adc2e6ce2be97bf616139453c3c
Mohanty <sm048q@intl.att.com>
2020-04-19 01:15:25 +0530

    service.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
  
 const app = express();
  
-// Serve only the static files form the covid19-tracker directory
-app.use(express.static(__dirname + '/covid19-tracker'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/angularapp'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/covid19-tracker/index.html'));
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
